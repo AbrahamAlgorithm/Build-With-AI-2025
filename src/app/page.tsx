0f507878399c6ef9500896e081e78e5d967993ce
AbrahamAlgorithm <abrahamfolorunso6@gmail.com>
2025-07-12 15:30:30 +0100

    fix: few tweaks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className="min-h-screen flex flex-col bg-white">
       <Header />
-      <main className="flex-1">{/* Your main content here */}</main>
+      <main className="flex-1">{/* main content here */}</main>
       <Footer />
     </div>
   );
