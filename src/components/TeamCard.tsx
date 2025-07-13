be83a44e63eb511f821a6297231281c6de0a8013
Alao Eniola <alaoeuniceeniola@gmail.com>
2025-07-13 21:55:41 +0100

    feat: add team page layout and components

---
+++ src/components/TeamCard.tsx
@@ -0,0 +1,28 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+type TeamCardProps = {
+  name: string;
+  title: string;
+  imageUrl: string;
+  socialMedia: string;
+  socialMediaLogo: string;
+};
+
+export default function TeamCard({ name, title, imageUrl, socialMedia, socialMediaLogo}: TeamCardProps) {
+  return (
+    <div className="circle">
+      <Image src={imageUrl} alt={name} width={180} height={180} />
+      <Link
+        href={socialMedia}
+        className="socials"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        X
+      </Link>
+      <h2 className="name">{name}</h2>
+      <p className="title">{title}</p>
+    </div>
+  );
+}
