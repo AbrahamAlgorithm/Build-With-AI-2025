e94e3788393cf0cbcb6f15364c33e4c2da0c1599
AbrahamAlgorithm <abrahamfolorunso6@gmail.com>
2025-07-21 06:52:44 +0100

    feat: made some mod

---
+++ src/app/team/teamdata.ts
@@ -0,0 +1,92 @@
+export const team = [
+  {
+    id: 1,
+    name: 'Abraham Folorunso',
+    title: 'Lead Organizer',
+    imageUrl: '/team-images/Abraham.png',
+    socialMedia:'https//',
+    socialMediaLogo: '/logo/WEB STUFF-04.png',
+    subtitle: 'Software Engineer'
+  },
+  {
+    id: 2,
+    name: 'Abdulsalam Abdulrasheed',
+    title: 'Bootcamp Lead',
+    imageUrl: '',
+    socialMedia:'https',
+    socialMediaLogo: '/logo/WEB STUFF-04.png',
+    subtitle: 'Software Engineer'
+  },
+  {
+    id: 3,
+    name: 'Fawziyyah Muhammad',
+    title: 'Event Lead',
+    imageUrl: '',
+    socialMedia:'https//',
+    socialMediaLogo: '/logo/WEB STUFF-04.png',
+    subtitle: 'Frontend Developer'
+  },
+    {
+    id: 4,
+    name: 'Eniola Alao',
+    title: 'UI/UX Lead',
+    imageUrl: '/team-images/Eniola.jpg',
+    socialMedia:'https//',
+    socialMediaLogo: '/logo/WEB STUFF-04.png',
+    subtitle: 'Frontend Developer'
+  },
+  {
+    id: 5,
+    name: 'Abundant Joel',
+    title: 'Graphics Lead I',
+    imageUrl: '',
+    socialMedia:'https//',
+    socialMediaLogo: '/logo/WEB STUFF-04.png',
+    subtitle: 'Graphics Designer'
+  },
+  {
+    id: 6,
+    name: 'Muhammed Oparemi',
+    title: 'Frontend Lead',
+    imageUrl: '',
+    socialMedia:'https//',
+    socialMediaLogo: '/logo/WEB STUFF-04.png',
+    subtitle: 'Frontend Developer'
+  },
+  {
+    id: 7,
+    name: 'Joel Kolawole Francis',
+    title: 'Graphics Lead',
+    imageUrl: '',
+    socialMedia:'https//',
+    socialMediaLogo: '/logo/WEB STUFF-04.png',
+    subtitle: 'Graphics Designer'
+  },
+  {
+    id: 8,
+    name: 'Abdulsamad Babangida',
+    title: 'Publicity Lead',
+    imageUrl: '',
+    socialMedia:'https//',
+    ocialMediaLogo: '/logo/WEB STUFF-04.png',
+    subtitle: 'Ethical Hacker'
+  },
+ {
+    id: 9,
+    name: 'Aishat Adebanjo',
+    title: 'Community Manager',
+    imageUrl: '',
+    socialMedia:'https//',
+    socialMediaLogo: '/logo/WEB STUFF-04.png',
+    subtitle: 'ML/AI Engineer'
+  },
+   {
+    id: 10,
+    name: 'Hussaini Ahmed',
+    title: 'Data Analysis Lead',
+    imageUrl: '',
+    socialMedia:'https//',
+    socialMediaLogo: '/logo/WEB STUFF-04.png',
+    subtitle: 'Data Analyst'
+  },
+];
